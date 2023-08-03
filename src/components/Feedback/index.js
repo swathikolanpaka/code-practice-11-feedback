1b6e453716c849dcf23c6ef3be46f7056d876d23
swathikolanpaka <swathie72@gamil.com>
2023-08-03 06:07:10 +0000

    solution

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -4,30 +4,65 @@ import {Component} from 'react'
 import './index.css'
 
 const Emoji = props => {
-  const {emoji} = props
-  const {imgUrl, name} = emoji
+  const {emoji, changeToLoveEmoji} = props
+  const {imageUrl, name} = emoji
+  console.log(name)
+
+  const loveEmoji = () => {
+    if (name === 'Happy') {
+      changeToLoveEmoji()
+    }
+  }
 
   return (
-    <li>
-      <img src={imgUrl} alt={name} />
-      <p>{name}</p>
+    <li className="emoji-container">
+      <div className="single-emoji">
+        <button type="button">
+          <img src={imageUrl} alt={name} onClick={loveEmoji} />
+        </button>
+        <p>{name}</p>
+      </div>
     </li>
   )
 }
 
 class Feedback extends Component {
+  state = {isClicked: true}
+
+  changeToLoveEmoji = () => {
+    this.setState({isClicked: false})
+  }
+
   render() {
+    const {isClicked} = this.state
     const {resources} = this.props
+    const {emojis, loveEmojiUrl} = resources
+    const headingText = isClicked
+      ? 'How satisfied are you with our customer support performance'
+      : 'Thank You'
+
     return (
       <div className="bg-container">
-        <div className="small-container">
-          <h1>How satisfied are you with our customer support performance</h1>
-          <ul className="emoji-container">
-            {resources.map(eachResource => (
-              <Emoji emoji={eachResource} key={eachResource.id} />
-            ))}
-          </ul>
-        </div>
+        {isClicked ? (
+          <div>
+            <h1>{headingText}</h1>
+            <ul className="emoji-container">
+              {emojis.map(eachEmoji => (
+                <Emoji
+                  emoji={eachEmoji}
+                  key={eachEmoji.id}
+                  changeToLoveEmoji={this.changeToLoveEmoji}
+                />
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div className="love-container">
+            <img src={loveEmojiUrl} alt="love emoji" />
+            <h1>{headingText}</h1>
+            <p>We will use your feedback yo improve</p>
+          </div>
+        )}
       </div>
     )
   }
